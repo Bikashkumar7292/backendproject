5320cfedb1211861568d7093e69103a8c9ab931e
Bikash Kumar <bkbikas0@gmail.com>
2024-10-30 18:50:11 +0530

    all set up config

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser"
 
 
 
+
 const app = express()
 app.use(cors({
 	origin: process.env.CORS_ORIGIN, credentials: true
@@ -15,6 +16,10 @@ app.use(express.json({
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+import userRouter from "./routes/user.routes.js"
+app.use("/api/v1/users", userRouter)
+
 
 
 export { app }
